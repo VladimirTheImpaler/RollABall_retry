7ddf57b358ccb77391fd6300be46e9190ebb79e0
VladimirTheImpaler <venturianfan098@gmail.com>
2023-01-27 16:32:14 -0600

    added pacer

--- Rollable/Assets/Scripts/playerController.cs
+++ Rollable/Assets/Scripts/playerController.cs
@@ -37,7 +37,7 @@ public class playerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 14)
         {
             winTextObject.SetActive(true);
         }
